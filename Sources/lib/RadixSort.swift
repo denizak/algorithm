48109ec062fe5b09e7024fa01ddf33d70fa86436
Deni <denizakya@gmail.com>
2020-01-02 11:59:10 +0700

    Update radix sort

--- Sources/lib/RadixSort.swift
+++ Sources/lib/RadixSort.swift
@@ -1,37 +1,28 @@
-private func countingSort(array: [Int], exp: Int) -> [Int] {
-
-    var output = [Int](repeating: 0, count: array.count)
-    var count = [Int](repeating: 0, count: 12)
-
-    var index = 0
-    while index < array.count {
-        let currentIndexCount = (array[index]/exp) % 10
-        count[currentIndexCount] += 1
-        index += 1
-    }
-
-    for i in 1..<12 {
-        count[i] += count[i-1]
-    }
-
-    var startIndex = array.count - 1
-    while startIndex >= 0 {
-        output[ count[ (array[startIndex]/exp)%10 ] - 1 ] = array[startIndex]
-        count[ (array[startIndex]/exp)%10] -= 1
-        startIndex -= 1
-    }
-
-    return output
-}
-
 func radixSorted(array: [Int]) -> [Int] {
-    let max = array.max()!
-    var result = array
-    var exp = 1
-    while max/exp > 0 {
-        result = countingSort(array: result, exp: exp)
-        exp *= 10
+    let base = 10
+    var finished = false
+    var digits = 1
+
+    var results = array
+    while !finished {
+        finished = true
+        var buckets: [[Int]] = .init(repeating: [], count: base)
+
+        results.forEach {
+            number in
+
+            let remainingPart = number / digits
+            let currentDigit = remainingPart % base
+
+            buckets[currentDigit].append(number)
+            if remainingPart > 0 {
+                finished = false
+            }
+        }
+
+        digits *= base
+        results = buckets.flatMap { $0 }
     }
 
-    return result
+    return results
 }
