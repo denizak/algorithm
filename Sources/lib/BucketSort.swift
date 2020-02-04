900f6c5f201ed69b4e2c3953c3eadfaf5057a5bf
Deni <denizakya@gmail.com>
2020-02-04 21:31:52 +0700

    simplify bucket sort

--- Sources/lib/BucketSort.swift
+++ Sources/lib/BucketSort.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 func bucketSorted(array: [Int]) -> [Int] {
+    if array.isEmpty { return array }
     let maxItem = array.max()!
     let minItem = array.min()!
     var placeholders = [Int: Bool]()
@@ -8,12 +9,7 @@ func bucketSorted(array: [Int]) -> [Int] {
     (minItem...maxItem).forEach { placeholders[$0] = false }
     array.forEach { placeholders[$0] = true }
 
-    var sortedArray = [Int]()
-    (minItem...maxItem)
-        .forEach {
-            if let value = placeholders[$0],
-                value { sortedArray.append($0) }
-    }
-
-    return sortedArray
+    return (minItem...maxItem)
+        .filter { placeholders[$0] != nil && placeholders[$0]! }
+        .compactMap { $0 }
 }
