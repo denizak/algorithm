16ac6c89a47af7bd8ac53bc451a973859dbc7d0f
Deni <denizakya@gmail.com>
2020-02-07 22:16:43 +0700

    Change package name to Sort

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Sort",
-            targets: ["lib"]),
+            targets: ["Sort"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "lib",
+            name: "Sort",
             dependencies: []),
         .testTarget(
             name: "libTests",
-            dependencies: ["lib"]),
+            dependencies: ["Sort"]),
     ]
 )
