d5fb3abe07fc99599431ae6fefa1e53d58ae91b1
Deni <denizakya@gmail.com>
2020-02-07 20:50:33 +0700

    Update name variable in Heap sort

--- Sources/lib/HeapSort.swift
+++ Sources/lib/HeapSort.swift
@@ -2,45 +2,40 @@ typealias Index = Int
 
 func heapSorted(items: [Int]) -> [Int] {
 
-    var resultItems = [Int]()
-
     var tempItems = items
-    let count = items.count / 2 - 1
-    for i in (0...count).reversed() {
-        tempItems = heapify(items: tempItems, n: items.count, i: i)
+    let count = tempItems.count / 2 - 1
+    for index in (0...count).reversed() {
+        tempItems = heapify(items: tempItems, count: tempItems.count, startIndex: index)
     }
 
-    for i in (0...tempItems.count-1).reversed() {
-        tempItems.swapAt(0, tempItems.count - 1)
-        resultItems.insert(tempItems.removeLast(), at: 0)
-
-        tempItems = heapify(items: tempItems, n: i, i: 0)
-    }
-
-    return resultItems
-}
-
-private func heapify(items: [Int], n: Int, i: Int) -> [Int] {
-
-    var tempItems = items
-    var largest = i
-    let left = leftChildIndex(of: i)
-    let right = rightChildIndex(of: i)
-    
-    if left < n && items[left] > items[largest] {
-        largest = left
-    }
-    if right < n && items[right] > items[largest] {
-        largest = right
-    }
- 
-    if largest != i {
-        tempItems.swapAt(i, largest)
-
-        return heapify(items: tempItems, n: n, i: largest)
+    for index in tempItems.indices.reversed() {
+        tempItems.swapAt(0, index)
+        tempItems = heapify(items: tempItems, count: index, startIndex: 0)
     }
 
     return tempItems
+}
+
+private func heapify(items: [Int], count: Int, startIndex: Index) -> [Int] {
+
+    var tempItems = items
+    var largest = startIndex
+    let left = leftChildIndex(of: startIndex)
+    let right = rightChildIndex(of: startIndex)
+    
+    if left < count && items[left] > items[largest] {
+        largest = left
+    }
+    if right < count && items[right] > items[largest] {
+        largest = right
+    }
+ 
+    if largest != startIndex {
+        tempItems.swapAt(startIndex, largest)
+        return heapify(items: tempItems, count: count, startIndex: largest)
+    } else {
+        return tempItems
+    }
 }
 
 private func leftChildIndex(of index: Index) -> Index {
